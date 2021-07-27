87a23f264c447abac06ae9aef731a62a6b1e1fca
rossbowen <ross.bowen@ons.gov.uk>
2021-07-27 15:51:58 +0100

    Initial commit

---
+++ _common.R
@@ -0,0 +1,15 @@
+set.seed(1014)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+# Activate crayon output
+options(
+  crayon.enabled = TRUE,
+  pillar.bold = TRUE,
+  stringr.html = FALSE
+)
